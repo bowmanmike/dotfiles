e91f3bc55d6c0c8e0e4db03c28dc60478f39e7be
Mike Bowman <mbowman2@gmail.com>
2020-04-13 10:47:28 -0400

    Update iex config

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,6 @@
-IEx.configure colors: [ eval_result: [ :cyan, :bright ] ]
+import_if_available(Ecto.Query)
+
+import_file("~/.iex/prompt_iex.exs")
+import_file("~/.iex/colours_iex.exs")
+import_file("~/.iex/inspect_iex.exs")
+import_file("~/.iex/aliases_iex.exs")
