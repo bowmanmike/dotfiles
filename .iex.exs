8e2158a9e4a558378448b354221f62d9b77d7246
Mike Bowman <mbowman2@gmail.com>
2021-02-28 20:44:59 -0500

    Use default iex colours

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,6 @@
 import_if_available(Ecto.Query)
 
 import_file("~/.iex/prompt_iex.exs")
-import_file("~/.iex/colours_iex.exs")
+# import_file("~/.iex/colours_iex.exs")
 import_file("~/.iex/inspect_iex.exs")
 import_file("~/.iex/aliases_iex.exs")
