4747187996bfe9c75347a3ad7fb3b8a57c3e1e07
Mike Bowman <mbowman2@gmail.com>
2021-10-12 11:43:50 -0400

    Close telescope on esc

--- lvim/config.lua
+++ lvim/config.lua
@@ -39,17 +39,18 @@ lvim.keys.term_mode["<C-o>"] = "<C-\\><C-n>"
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+lvim.builtin.telescope.on_config_done = function()
+  local actions = require "telescope.actions"
+  lvim.builtin.telescope.defaults.mappings.i["<esc>"] = actions.close
+  -- for input mode
+  -- lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+  -- lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+  -- lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+  -- lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+  -- for normal mode
+  -- lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+  -- lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+end
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.telescope.defaults.path_display = function(opts, path)
