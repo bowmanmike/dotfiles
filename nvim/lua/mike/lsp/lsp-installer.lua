58dacb098dd6f9f79656d0e93340d7d61d971914
Mike Bowman <mbowman2@gmail.com>
2022-06-01 13:55:31 -0400

    Refactor lsp setup

--- nvim/lua/mike/lsp/lsp-installer.lua
+++ nvim/lua/mike/lsp/lsp-installer.lua
@@ -1,142 +1,129 @@
 local lsp_installer = require("nvim-lsp-installer")
 local handlers = require("mike.lsp.handlers")
--- require("nvim-lsp-installer").settings { log_level = vim.log.levels.DEBUG }
+local lspconfig = require("lspconfig")
 
-lsp_installer.on_server_ready(function(server)
-	local default_opts = {
-		on_attach = handlers.on_attach,
-		capabilities = handlers.capabilities,
-	}
+lsp_installer.setup()
 
-	-- JSON schemas
-	-- local nlsp_default_schemas = require("nlspsettings.jsonls").get_default_schemas()
-	local nlsp_default_schemas = require("nlspsettings").get_default_schemas()
-	local schemastore_schemas = require("schemastore").json.schemas({
-		select = {
-			"package.json",
-			".eslintrc",
-			"tsconfig.json",
+local default_opts = {
+	on_attach = handlers.on_attach,
+	capabilities = handlers.capabilities,
+}
+
+lspconfig.sumneko_lua.setup({
+	on_attach = default_opts.on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
 		},
-	})
-	local json_schemas = require("mike.utils").table_merge(nlsp_default_schemas, schemastore_schemas)
+	},
+})
 
-	-- Set custom server config here
-	local server_opts = {
-		sumneko_lua = function()
-			-- default_opts.cmd = { sumneko_binary, "-E", sumneko_root_path .. "/extension/server/main.lua" }
+local nlsp_default_schemas = require("nlspsettings").get_default_schemas()
+local schemastore_schemas = require("schemastore").json.schemas({
+	select = {
+		"package.json",
+		".eslintrc",
+		"tsconfig.json",
+	},
+})
+local json_schemas = require("mike.utils").table_merge(nlsp_default_schemas, schemastore_schemas)
 
-			default_opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+lspconfig.jsonls.setup({
+	on_attach = default_opts.on_attach,
+	settings = {
+		json = {
+			schemas = json_schemas,
+		},
+	},
+})
 
-			default_opts.on_attach = handlers.on_attach
-			default_opts.capabilities = handlers.capabilities
-		end,
-		jsonls = function()
-			default_opts.settings = {
-				json = {
-					schemas = json_schemas,
-				},
-			}
-			default_opts.on_attach = handlers.on_attach
-			default_opts.capabilities = handlers.capabilities
-		end,
-		-- eslint = function()
-		-- 	default_opts.on_attach = function(client, bufnr)
-		-- 		client.resolved_capabilities.document_formatting = true
-		-- 		handlers.on_attach(client, bufnr)
-		-- 	end
-		-- 	default_opts.settings = {
-		-- 		format = { enable = false },
-		-- 	}
-		-- end,
-		tsserver = function()
-			default_opts.on_attach = function(client, bufnr)
-				-- client.resolved_capabilities.document_formatting = false
-				handlers.on_attach(client, bufnr)
-			end
-		end,
-		solargraph = function()
-			default_opts.on_attach = function(client, bufnr)
-				-- client.resolved_capabilities.document_formatting = false
-				client.resolved_capabilities.diagnostics = true
-				-- client.diagnostics = false
-				handlers.on_attach(client, bufnr)
-				-- default_opts.settings.solargraph.diagnostics = false
-			end
-			-- print("HERE")
-			-- default_opts.settings.diagnostics = false
-		end,
-		stylelint_lsp = function()
-			default_opts.on_attach = function(client, bufnr)
-				-- client.resolved_capabilities.document_formatting = false
-				handlers.on_attach(client, bufnr)
-			end
-		end,
-		tailwindcss = function()
-			return {
-				cmd = { "tailwindcss-language-server", "--stdio" },
-				init_options = {
-					userLanguages = {
-						elixir = "phoenix-heex",
-						eruby = "erb",
-						heex = "phoenix-heex",
-            svelte = "html"
-					},
-				},
-				handlers = {
-					["tailwindcss/getConfiguration"] = function(_, _, params, _, bufnr, _)
-						vim.lsp.buf_notify(bufnr, "tailwindcss/getConfigurationResponse", { _id = params._id })
-					end,
-				},
-				settings = {
-					includeLanguages = {
-						typescript = "javascript",
-						typescriptreact = "javascript",
-						["html-eex"] = "html",
-						["phoenix-heex"] = "html",
-						heex = "html",
-						eelixir = "html",
-						elm = "html",
-						erb = "html",
-            svelte = "html"
-					},
-					tailwindCSS = {
-						lint = {
-							cssConflict = "warning",
-							invalidApply = "error",
-							invalidConfigPath = "error",
-							invalidScreen = "error",
-							invalidTailwindDirective = "error",
-							invalidVariant = "error",
-							recommendedVariantOrder = "warning",
-						},
-						experimental = {
-							classRegex = {
-								[[class= "([^"]*)]],
-								[[class: "([^"]*)]],
-								'~H""".*class="([^"]*)".*"""',
-							},
-						},
-						validate = true,
-					},
-				},
-				filetypes = {
-					"css",
-					"scss",
-					"sass",
-					"html",
-					"heex",
-					"elixir",
-          "eruby",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-          "svelte",
-				},
-			}
-		end,
-	}
+lspconfig.tsserver.setup({
+	on_attach = function(client, bufnr)
+		client.server_capabilities.document_formatting = false
+		default_opts.on_attach(client, bufnr)
+	end,
+})
 
-	server:setup((server_opts[server.name] and server_opts[server.name]()) or default_opts)
-	vim.cmd([[ do User LspAttachBuffers ]])
-end)
+lspconfig.solargraph.setup({
+	on_attach = function(client, bufnr)
+		client.server_capabilities.diagnostics = true
+		default_opts.on_attach(client, bufnr)
+	end,
+})
+
+lspconfig.stylelint_lsp.setup({
+	on_attach = default_opts.on_attach,
+})
+
+lspconfig.tailwindcss.setup({
+	init_options = {
+		userLanguages = {
+			elixir = "phoenix-heex",
+			eruby = "erb",
+			heex = "phoenix-heex",
+			svelte = "html",
+		},
+	},
+	handlers = {
+		["tailwindcss/getConfiguration"] = function(_, _, params, _, bufnr, _)
+			vim.lsp.buf_notify(bufnr, "tailwindcss/getConfigurationResponse", { _id = params._id })
+		end,
+	},
+	settings = {
+		includeLanguages = {
+			typescript = "javascript",
+			typescriptreact = "javascript",
+			["html-eex"] = "html",
+			["phoenix-heex"] = "html",
+			heex = "html",
+			eelixir = "html",
+			elm = "html",
+			erb = "html",
+			svelte = "html",
+		},
+		tailwindCSS = {
+			lint = {
+				cssConflict = "warning",
+				invalidApply = "error",
+				invalidConfigPath = "error",
+				invalidScreen = "error",
+				invalidTailwindDirective = "error",
+				invalidVariant = "error",
+				recommendedVariantOrder = "warning",
+			},
+			experimental = {
+				classRegex = {
+					[[class= "([^"]*)]],
+					[[class: "([^"]*)]],
+					'~H""".*class="([^"]*)".*"""',
+				},
+			},
+			validate = true,
+		},
+	},
+	filetypes = {
+		"css",
+		"scss",
+		"sass",
+		"html",
+		"heex",
+		"elixir",
+		"eruby",
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"svelte",
+	},
+	on_attach = default_opts.on_attach,
+})
+
+lspconfig.cssls.setup({ on_attach = default_opts.on_attach })
+lspconfig.elixirls.setup({ on_attach = default_opts.on_attach })
+lspconfig.graphql.setup({ on_attach = default_opts.on_attach })
+lspconfig.html.setup({ on_attach = default_opts.on_attach })
+lspconfig.prismals.setup({ on_attach = default_opts.on_attach })
+lspconfig.svelte.setup({ on_attach = default_opts.on_attach })
+lspconfig.volar.setup({ on_attach = default_opts.on_attach })
