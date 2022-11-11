7c663a362f2d90a1dc988976e6e34031276f0415
Mike Bowman <mike@mikebowman.dev>
2022-11-10 19:30:30 -0500

    Swap fzf for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,9 +2,9 @@ local telescope = require("telescope")
 telescope.setup({})
 telescope.load_extension("fzf")
 
--- vim.api.nvim_set_keymap("n", "<C-P>", ":Telescope find_files<cr>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<C-B>", ":Telescope buffers<cr>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<C-T>", ":Telescope live_grep<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-P>", ":Telescope find_files<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-B>", ":Telescope buffers<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-T>", ":Telescope live_grep<cr>", { noremap = true, silent = true })
 
 -- Colorscheme with preview
 -- <cmd>lua require('telescope.builtin.internal').colorscheme({enable_preview = true})<cr>
--- nvim/lua/mike/lsp_handlers.lua
+++ nvim/lua/mike/lsp_handlers.lua
@@ -1,8 +1,5 @@
 local M = {
   on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
