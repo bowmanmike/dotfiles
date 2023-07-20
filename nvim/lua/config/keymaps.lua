c2ea24ac48658b74c22abf9e1f0237b93f98370d
Mike Bowman <mike@mikebowman.dev>
2023-07-20 10:35:18 -0400

    Update plugins, add keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,32 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("t", "<C-o>", "<C-\\><C-n>", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+vim.cmd([[
+  function PrettyPrintJSON()
+    :%!jq '.' -M
+  endfunction
+
+  function! MinifyJSON()
+    :%!jq '.' -cM
+  endfunction
+
+  autocmd FileType json nmap <leader>pj :call PrettyPrintJSON()<cr>
+  autocmd FileType json nmap <leader>mj :call MinifyJSON()<cr>
+]])
+
+vim.cmd([[
+command! Q q
+command! W w
+command! Wq wq
+command! WQ wq
+command! Qw wq
+command! QW wq
+command! SO luafile $MYVIMRC
+command! SF luafile %
+]])
+
+vim.api.nvim_set_keymap("n", "<leader>cf", ':let @+ = expand("%")<cr>', { noremap = true, silent = true })
