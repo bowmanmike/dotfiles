403a4aa62cd4c355db62327c4d585fcaf89a9bf2
Mike Bowman <mike@mikebowman.dev>
2023-07-19 21:54:36 -0400

    Fugitive

--- nvim/lua/plugins/tpope.lua
+++ nvim/lua/plugins/tpope.lua
@@ -1,4 +1,9 @@
 return {
   "tpope/vim-surround",
-  "tpope/vim-fugitive",
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "n", "<leader>gs", "<cmd>Git<cr>", { silent = true, noremap = true } },
+    },
+  },
 }
