dafd5c728862e8a427593bb43c117f2a841198ba
Mike Bowman <mike@mikebowman.dev>
2023-11-05 15:09:28 -0500

    Updates

--- nvim/lua/plugins/tailwind.lua
+++ nvim/lua/plugins/tailwind.lua
@@ -7,6 +7,24 @@ return {
           filetypes_include = { "eruby" },
         },
       },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          -- Add default filetypes
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          -- Remove excluded filetypes
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          -- Add additional filetypes
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
+      },
     },
   },
   {
