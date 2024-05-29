fcd317dc4674594024566a7d87c8b40f0ddf7844
Mike Bowman <mike@mikebowman.dev>
2024-05-29 13:17:03 -0400

    Use onedark

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
-return { "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup()
-    vim.cmd.colorscheme "catppuccin"
-  end
+return { "catppuccin/nvim"
+--  name = "catppuccin",
+--  priority = 1000,
+--  config = function()
+--    require("catppuccin").setup()
+--    vim.cmd.colorscheme "catppuccin"
+--  end
 }
