6d8c25fc25a458a1ec21b969d357542a178294db
Mike Bowman <mike@mikebowman.dev>
2024-10-08 15:11:34 -0400

    Disable previous pane mapping

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,27 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+		vim.g.tmux_navigator_save_on_switch = 2
+	end,
+	config = function()
+		-- vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { silent = true })
+		-- vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { silent = true })
+		-- vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { silent = true })
+		-- vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { silent = true })
+	end,
 	cmd = {
 		"TmuxNavigateLeft",
 		"TmuxNavigateDown",
 		"TmuxNavigateUp",
 		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
+		-- "TmuxNavigatePrevious",
 	},
 	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { silent = true } },
+		{ "<c-j>", "<cmd>TmuxNavigateDown<cr>", { silent = true } },
+		{ "<c-k>", "<cmd>TmuxNavigateUp<cr>", { silent = true } },
+		{ "<c-l>", "<cmd>TmuxNavigateRight<cr>", { silent = true } },
+		-- 	-- { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 	},
 }
