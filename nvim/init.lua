1a40b9e85e9fbce5a70e994eaabca1d8c9b16e00
Mike Bowman <mike@mikebowman.dev>
2024-10-17 10:49:30 -0400

    Add lua filetype config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("vim-options")
+require("filetypes")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
---
+++ nvim/lua/filetypes.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
+})
