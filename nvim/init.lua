41962b32b2c34687117535228ed7140223f49c72
Mike Bowman <mike@mikebowman.dev>
2024-11-08 18:10:54 -0500

    Don't autoreload config on changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	change_detection = {
+		enabled = false,
+	},
+})
 
 require("misc")
