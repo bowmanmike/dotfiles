3c015e47001dab7c05b2a235da5f1b487ceb3373
Mike Bowman <mike.bowman@gusto.com>
2024-12-19 11:09:19 -0500

    Big update to fzf-lua config

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,38 +1,49 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons", { "junegunn/fzf", build = "./install --bin" } },
-		lazy = false,
-		config = function()
-			-- calling `setup` is optional for customization
-			local fzf_lua = require("fzf-lua")
+		config = function(_, opts)
+			require("fzf-lua").setup(opts)
 
-			fzf_lua.setup({ fzf_opts = { ["--layout"] = "default" } })
-
-			vim.keymap.set("n", "<c-p>", fzf_lua.files, { noremap = true, silent = true, desc = "FZF Files" })
-			vim.keymap.set("n", "<c-b>", fzf_lua.buffers, { noremap = true, silent = true, desc = "FZF Files" })
-			vim.keymap.set(
-				"n",
-				"<leader><leader>",
-				fzf_lua.oldfiles,
-				{ noremap = true, silent = true, desc = "FZF Old Files" }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>rr",
-				fzf_lua.lsp_references,
-				{ noremap = true, silent = true, desc = "FZF Old Files" }
-			)
-			vim.keymap.set("n", "<leader>fh", fzf_lua.help_tags, { noremap = true, silent = true, desc = "FZF Help" })
-			vim.keymap.set(
-				"n",
-				"<leader>tg",
-				fzf_lua.git_status,
-				{ noremap = true, silent = true, desc = "FZF Git Status" }
-			)
-
-			vim.api.nvim_create_user_command("Rg", fzf_lua.live_grep, { desc = "Search project by grep" })
+			vim.api.nvim_create_user_command("Rg", require("fzf-lua").live_grep, { desc = "Search project by grep" })
 		end,
+		cmd = "Rg",
+		keys = {
+			{ "<c-p>", "<cmd>FzfLua files<cr>", { noremap = true, silent = true, desc = "FZF Files" } },
+			{ "<c-b>", "<cmd>FzfLua buffers<cr>", { noremap = true, silent = true, desc = "FZF Buffers" } },
+			{ "<c-t>", "<cmd>FzfLua live_grep<cr>", { noremap = true, silent = true, desc = "FZF Buffers" } },
+			{
+				"<leader><leader>",
+				"<cmd>FzfLua oldfiles<cr>",
+				{ noremap = true, silent = true, desc = "FZF History" },
+			},
+			{
+				"<leader>rr",
+				"<cmd>FzfLua lsp_references<cr>",
+				{ noremap = true, silent = true, desc = "FZF LSP References" },
+			},
+			{ "<leader>fh", "<cmd>FzfLua help_tags<cr>", { noremap = true, silent = true, desc = "FZF Help" } },
+			-- {
+			-- 	"<leader>gt",
+			-- 	"<cmd>FzfLua git_status<cr>",
+			-- 	{ noremap = true, silent = true, desc = "FZF Git Status" },
+			-- },
+		},
+		opts = {
+			oldfiles = {
+				include_current_session = true,
+				cwd_only = true,
+				stat_file = true,
+			},
+			previewers = {
+				builtin = {
+					-- don't try to highlight files over 100kb
+					syntax_limit_b = 1024 * 100, -- 100KB
+				},
+			},
+			fzf_opts = {
+				["--layout"] = "default",
+			},
+		},
 	},
 }
