88f63ca5d2101922ce12e90f12f636a7cb1fd1a2
Mike Bowman <mike.bowman@gusto.com>
2025-02-06 14:41:53 -0500

    Use default ghostty theme, add harpoon keybinds

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -19,5 +19,21 @@ return {
 		vim.keymap.set("n", "ghn", function()
 			harpoon:list():next({ ui_nav_wrap = true })
 		end)
+
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				vim.keymap.set("n", "<C-v>", function()
+					harpoon.ui:select_menu_item({ vsplit = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-x>", function()
+					harpoon.ui:select_menu_item({ split = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon.ui:select_menu_item({ tabedit = true })
+				end, { buffer = cx.bufnr })
+			end,
+		})
 	end,
 }
