c0478c429352297beb69f51c43a8fb207e05206d
Mike Bowman <mike@mikebowman.dev>
2025-10-14 18:52:47 -0400

    Lock blink version, update lazy-lock

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -98,7 +98,7 @@ return {
 		},
 
 		-- use a release tag to download pre-built binaries
-		version = "1.*",
+		version = "1.7",
 		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 		-- build = 'cargo build --release',
 		-- If you use nix, you can build from source using latest nightly rust with:
