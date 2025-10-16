dfa1928504b9bc8a7469696f16d98f2ba12c7654
Mike Bowman <mike@mikebowman.dev>
2025-10-16 16:43:57 -0400

    Show docs in blink window

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -132,7 +132,7 @@ return {
 
 			-- (Default) Only show the documentation popup when manually triggered
 			completion = {
-				documentation = { auto_show = false },
+				documentation = { auto_show = true },
 				ghost_text = { enabled = true },
 				list = { selection = {
 					preselect = true,
